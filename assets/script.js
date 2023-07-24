a37bd308094a488cc232427f8c14dd064abab806
Ranjeet poojari <ranjeetpoojari77@gmail.com>
2023-07-24 16:51:50 +0530

    new update

--- assets/script.js
+++ assets/script.js
@@ -5,28 +5,33 @@ $(document).ready(function() {
         event.preventDefault()
         var emailphone = $('#exampleInputEmail1').val();
         var password = $('#passwordInput').val();
-        var emailpattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        var num_pattern = /^\d{10}$/;
+        // var emailpattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        // var num_pattern = /^\d{10}$/;
         let isValid = true;
         if(emailphone.trim() === ''){
             $('#emailError').text('Email id or phone number is required.');
+            $('#exampleInputEmail1').addClass('border-danger');
             isValid = false;
         }
          else {
             $('#emailError').text('');
+            $('#exampleInputEmail1').removeClass('border-danger');
             isValid = true;
                 console.log('done');
         }
         if(password.trim() === ''){
             $('#passwordError').text('The pass word  is required.');
+            $('#passwordInput').addClass('border-danger');
             isValid = false;
         }
         else if(password.length <= 8 ){
             $('#passwordError').text('The password must be 8 charters');
+            $('#passwordInput').addClass('border-danger');
             isValid = false;
         }
         else{
             console.log('done');
+            $('#passwordInput').removeClass('border-danger');
         }
         if(isValid){
             console.log("login submit");
@@ -44,38 +49,46 @@ $(document).ready(function() {
         let isValid = true;
         if(name.trim() === ''){
             $('#fullnameerror').text('Full name is required');
+            $('#Fullname').addClass('border-danger');
              isValid =false
         }
         else if(!regEx.test(name)){
-            
+            $('#Fullname').addClass('border-danger');
             $('#fullnameerror').text('Please provide Valid name');
             isValid = false
         } 
         else{
             $('#fullnameerror').text('');
+            $('#Fullname').removeClass('border-danger');
             isValid = true
             console.log('done');
         }
         if(emailphone.trim() === ''){
             $('#emailError').text('Email id or phone number is required.');
+            $('#exampleInputEmail1').addClass('border-danger');
             isValid = false;
         }
          else {
+            $('#emailError').text('');
+            $('#exampleInputEmail1').removeClass('border-danger');
+            isValid = true;
                 console.log('done');
         }
         if(password.trim() === ''){
-            $('#passwordError').text('The pass word is required.');
+            $('#passwordError').text('The pass word  is required.');
+            $('#passwordInput').addClass('border-danger');
             isValid = false;
         }
         else if(password.length <= 8 ){
             $('#passwordError').text('The password must be 8 charters');
+            $('#passwordInput').addClass('border-danger');
             isValid = false;
         }
         else{
             console.log('done');
+            $('#passwordInput').removeClass('border-danger');
         }
-        if(password.trim() === ''){
-        }
+      
         if(isValid){
             console.log("login submit");
         }
@@ -89,23 +102,27 @@ $(document).ready(function() {
         var regEx = /^[A-Za-z]+$/;
         let isValid = true;
         if(addreess.trim() === ''){
-            console.log("not Yet addree")
+            $('#addreess').addClass('border-danger');
             $('#addressError').text("Address is required");
             isValid = false;
         }
         else{
+            $('#addreess').removeClass('border-danger');
             console.log('done');
         }
         if(cover_Location.trim() === ''){
             console.log("not yet");
             $('#cover_LocationError').text('Location will cover for puja is required.');
+            $('#cover_Location').addClass('border-danger');
             isValid = false;
         }else if(!regEx.test(cover_Location)){
             $('#cover_LocationError').text('Please Provide Your Currect Location');
+            $('#cover_Location').addClass('border-danger');
             isValid = false;
         }
-         else {
+        else {
             isValid =true;
+            $('#cover_Location').removeClass('border-danger');
             $('#cover_LocationError').text('');
         }
         if(isValid){
@@ -151,32 +168,41 @@ $(document).ready(function(){
         if(reference.trim() ===''){
         // console.log("good night");
         $("#referenceError").text("Reference ID is required");  
+        $('#referenceid').addClass('border-danger');
+        
         isValid = false;
     }
     else if(!regEx.test(reference)){
         $("#referenceError").text("Only Numbers");  
+        $('#referenceid').addClass('border-danger');
         isValid = false;
     }
     else{
         isValid = true;
         // console.log("good morning");
+        $('#referenceid').removeClass('border-danger');
         $("#referenceError").text(""); 
     }
     if(total_no_puja.trim() ===""){
-      
+        
         $("#total_no_pujaError").text("Reference ID is required");  
+        $('#total_no_puja').addClass('border-danger');
         isValid = false;
     }else if(!regEx.test(total_no_puja)){
         $("#total_no_pujaError").text("Only numbers");  
+        $('#total_no_puja').addClass('border-danger');
         isValid = false;
     }
     else{
+        $('#total_no_puja').removeClass('border-danger');
         $("#total_no_pujaError").text("");
         isValid = true;
     }
     var isLanguage_preference = $(".Language_preference:checked").length ;
     if(isLanguage_preference < 1){
         isValid = false;
+        $('.Language_preference').addClass('border-danger');
+
          $('#Language_preferenceError').text('Expertise in puja is required');
         }
         else{
